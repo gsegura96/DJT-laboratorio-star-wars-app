d3247dc042d1dc0b12c6a418391d08cc9ca89993
Daniel <daniel.esteban.8@hotmail.com>
2021-09-03 21:46:48 -0600

    New Branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,24 +9,6 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 
 
-//Example
-import javax.swing.*;    
-public class TableExample {    
-    JFrame f;    
-    TableExample(){    
-    f=new JFrame();    
-    String data[][]={ {"101","Amit","670000"},    
-                          {"102","Jai","780000"},    
-                          {"101","Sachin","700000"}};    
-    String column[]={"ID","NAME","SALARY"};         
-    JTable jt=new JTable(data,column);    
-    jt.setBounds(30,40,200,300);          
-    JScrollPane sp=new JScrollPane(jt);    
-    f.add(sp);          
-    f.setSize(300,400);    
-    f.setVisible(true);    
-}      
-
 public class Main {
     private static ObjectMapper mapper = new ObjectMapper();
 
@@ -55,6 +37,5 @@ public class Main {
         } finally {
             httpClient.close();
         }
-    }
-    new TableExample();    
+    }  
 }
