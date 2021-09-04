b6253ed90b228bfe7e242b1d72924708d3742085
Gustavo Segura <yo@gustavosegura.com>
2021-09-03 18:16:08 -0600

    unit test

---
+++ src/test/java/test1.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+// import org.junit.jupiter.api.Test;
+ 
+class JUnit5ExampleTest {
+ 
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+        assertEquals(1, 1);
+
+    }
+}
